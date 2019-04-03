d610bc67b2f4c1240d93385599e5ca1b96f8a16d
Tim Harley <Timothy.D.Harley@gmail.com>
2019-04-02 20:20:02 -0500

    Add project files.

---
+++ BadSuperbOwlNamer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BadSuperbOwlNamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
